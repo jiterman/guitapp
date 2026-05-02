71cb43a853d7679326972b46326ddeb44ffcbb64
Bruno Quiroga <bquiroga@fi.uba.ar>
2026-05-02 20:38:12 -0300

    Print del codigo de confirmacion de registro

--- backend/src/main/java/org/fiuba/guitapp/GuitappApplication.java
+++ backend/src/main/java/org/fiuba/guitapp/GuitappApplication.java
@@ -1,5 +1,7 @@
 package org.fiuba.guitapp;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GuitappApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(GuitappApplication.class, args);
     }
 }
