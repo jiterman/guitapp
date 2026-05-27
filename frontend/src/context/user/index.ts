9fb1411c35018d92dbb552ebfe23c957da135edb
christian fernandez <cefernandez@fi.uba.ar>
2026-05-26 22:40:35 -0300

    fix linter

--- frontend/src/context/user/index.ts
+++ frontend/src/context/user/index.ts
@@ -1,4 +1,4 @@
 export { UserProvider } from './provider';
 export { useUser } from './useUser';
-export type { UserContextType } from './context';
 export { UserContext } from './context';
+export type { UserProfile } from './context';
